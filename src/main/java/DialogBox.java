af8ddb1103861777604e73a60d536cea30228b7e
zuoshihua <zuoshihua@proton.me>
2025-02-05 00:04:26 +0800

    Finish JavaFX tutorial part 2 - Styling the Controls

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,3 +1,4 @@
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -9,8 +10,15 @@ public class DialogBox extends HBox {
     private ImageView displayPicture;
 
     public DialogBox(String s, Image i) {
-        text = new Label();
+        text = new Label(s);
         displayPicture = new ImageView(i);
+
+        // Styling the dialog box
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        this.setAlignment(Pos.TOP_RIGHT);
+
         this.getChildren().addAll(text, displayPicture);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -22,8 +23,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Setting up required components
 
+        // Setting up required components
         scrollPane = new ScrollPane();
         dialogContainer = new VBox();
         scrollPane.setContent(dialogContainer);
@@ -38,6 +39,35 @@ public class Main extends Application {
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
         scene = new Scene(mainLayout);
+
+        // Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        
+        userInput.setPrefWidth(325.0);
+        
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput , 1.0);
 
         stage.setScene(scene);
         stage.show();
