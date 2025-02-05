1e69cdfb2cf239fe6ac12624f0574108fb140706
zuoshihua <zuoshihua@proton.me>
2025-02-05 20:43:44 +0800

    Finish JavaFX tutorial part 5

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -43,6 +43,7 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
